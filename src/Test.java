45d3121af184e35152ccb85b348fb8c30936e535
Haru <1196756653@qq.com>
2017-06-01 18:25:35 +0800

    问题 如何模块匹配到缩放后的图片

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,16 @@
 import org.opencv.core.Core;
 
+import com.reharu.opencv.OpencvImage;
+
 public class Test {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		TemplateMaching maching = new TemplateMaching() ;
-		maching.setSource("timg.jpg");
-		maching.setDst("timgA.jpg");
-		maching.process();
+//		TemplateMaching maching = new TemplateMaching("imgs/homePage/") ;
+//		maching.setSource("bai_lian.png");
+//		maching.setDst("bai_lianA.png");
+//		maching.process();
+		OpencvImage src = new OpencvImage("imgs/homePage/fei_hong_b.png") ;
+		OpencvImage dst = new OpencvImage("imgs/characters/fei_hong.png") ;
+		System.out.println(src.getArea(dst));
 	}
 }
